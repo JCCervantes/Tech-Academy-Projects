ca3b7c2a17c7026189390a1cd2f45b0f580d8ac0
JCCervantes <julia.c.cervantes@gmail.com>
2017-10-14 15:36:54 -0700

    Radio Button Challenge

---
+++ C# Projects/RadioButtonChallenge/RadioButtonChallenge/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RadioButtonChallenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Your note taking preferences:<br />
+            <br />
+            <asp:RadioButton ID="RadioButton1" runat="server" GroupName="NP" Text="Pencil" />
+        </div>
+        <p>
+            <asp:RadioButton ID="RadioButton2" runat="server" GroupName="NP" Text="Pen" />
+        </p>
+        <p>
+            <asp:RadioButton ID="RadioButton3" runat="server" GroupName="NP" Text="Phone" />
+        </p>
+        <p>
+            <asp:RadioButton ID="RadioButton4" runat="server" GroupName="NP" Text="Tablet" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="OKButton" runat="server" OnClick="OKButton_Click" Text=" OK  " />
+        </p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+        <p>
+            <asp:Image ID="resultImage" runat="server" Width="16px" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ C# Projects/RadioButtonChallenge/RadioButtonChallenge/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RadioButtonChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void OKButton_Click(object sender, EventArgs e)
+        {/*
+            if (RadioButton1.Checked)
+                resultLabel.Text = "You selected Pencil";
+            else if (RadioButton2.Checked)
+                resultLabel.Text = "You selected Pen";
+            else if (RadioButton3.Checked)
+                resultLabel.Text = "You selected Phone";
+            else if (RadioButton4.Checked)
+                resultLabel.Text = "You selected Tablet";
+            else resultLabel.Text = "Please select an option";
+        */
+            if (RadioButton1.Checked)
+                resultImage.ImageUrl = "pencil.png";
+            else if (RadioButton2.Checked)
+                resultImage.ImageUrl = "pen.png";
+            else if (RadioButton3.Checked)
+                resultImage.ImageUrl = "phone.png";
+            else if (RadioButton4.Checked)
+                resultImage.ImageUrl = "tablet.png";
+            else resultLabel.Text = "Please select an option";
+
+        }
+    }
+}
