0c4f1703b7e29784cf1c8b5dd3ae9529fb0492f0
JCCervantes <julia.c.cervantes@gmail.com>
2017-10-24 21:33:38 -0700

    PostalCalculatorChallenge

---
+++ C# Visual Studio Projects/PostalCalculatorHelperMethods/PostalCalculatorHelperMethods/Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PostalCalculatorHelperMethods.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Postal Calculator
+            <br />
+            <br />
+            Width:
+            <asp:TextBox ID="Width" runat="server" AutoPostBack="True" OnTextChanged="Width_TextChanged"></asp:TextBox>
+            <br />
+            Height:
+            <asp:TextBox ID="Height" runat="server" AutoPostBack="True" OnTextChanged="Height_TextChanged"></asp:TextBox>
+            <br />
+            Length:
+            <asp:TextBox ID="Length" runat="server" AutoPostBack="True" OnTextChanged="Length_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <asp:RadioButton ID="GroundButton" runat="server" AutoPostBack="True" GroupName="Shipping" OnCheckedChanged="GroundButton_CheckedChanged" Text="Ground" />
+            <br />
+            <asp:RadioButton ID="AirButton" runat="server" AutoPostBack="True" GroupName="Shipping" OnCheckedChanged="AirButton_CheckedChanged" Text="Air" />
+            <br />
+            <asp:RadioButton ID="NextDayButton" runat="server" AutoPostBack="True" GroupName="Shipping" OnCheckedChanged="NextDayButton_CheckedChanged" Text="Next Day" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ C# Visual Studio Projects/PostalCalculatorHelperMethods/PostalCalculatorHelperMethods/Default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PostalCalculatorHelperMethods
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Width_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Height_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Length_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GroundButton_CheckedChanged(object sender, EventArgs e)
+        {
+            calculatePostage(.15);
+        }
+
+        protected void AirButton_CheckedChanged(object sender, EventArgs e)
+        {
+            calculatePostage(.25);
+        }
+
+        protected void NextDayButton_CheckedChanged(object sender, EventArgs e)
+        {
+            calculatePostage(.45);
+        }
+
+        protected void calculatePostage(double multiplier)
+        {
+            if ((Width.Text.Trim().Length == 0) || (Height.Text.Trim().Length == 0) || (Length.Text.Trim().Length == 0))
+            {
+                resultLabel.Text = "You must enter the package dimensions.";
+                return;
+            } 
+            else
+	        {
+                int volume = (int.Parse(Width.Text)) * (int.Parse(Height.Text)) * (int.Parse(Length.Text));
+
+                double Postage = volume * multiplier;
+
+                resultLabel.Text = String.Format("The postage for this package is {0:C}.", Postage);
+            }
+
+        }
+
+    }
+}
